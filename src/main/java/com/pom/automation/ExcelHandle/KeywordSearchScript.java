e0041086ee844710dcffd33e1e56817d14694925
Kaizar Akib Ayon <kizar.cse@gmail.com>
2023-06-06 00:32:26 +0600

    Update some variable

--- src/main/java/com/pom/automation/ExcelHandle/KeywordSearchScript.java
+++ src/main/java/com/pom/automation/ExcelHandle/KeywordSearchScript.java
@@ -44,41 +44,40 @@ public class KeywordSearchScript {
             
             Sheet worksheet = workbook.getSheet(dayOfWeek.toString());
 
-            // Initialize the Chrome driver
-            //WebDriver driver = new ChromeDriver();
+            
             
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--remote-allow-origins=*");
             WebDriver driver = new ChromeDriver(options);
 
-            // Iterate over the rows in the worksheet starting from the second row (assuming headers are in the first row)
+           
             Iterator<Row> rowIterator = worksheet.iterator();
-            rowIterator.next(); // Skip the header row
+            rowIterator.next(); 
             while (rowIterator.hasNext()) {
                 Row row = rowIterator.next();
                 String keyword = row.getCell(0).getStringCellValue();
 
-                // Search the keyword in Google
+                
                 driver.get("https://www.google.com");
                 WebElement searchBox = driver.findElement(By.name("q"));
                 searchBox.sendKeys(keyword);
                 searchBox.sendKeys(Keys.RETURN);
 
-                // Find the longest and shortest options
+                
                 WebElement firstResult = driver.findElement(By.cssSelector("div.g:first-child h3"));
                 WebElement lastResult = driver.findElement(By.cssSelector("div.g:last-child h3"));
 
                 String longestOption = firstResult.getText();
                 String shortestOption = lastResult.getText();
 
-                // Write the longest and shortest options to the respective cells in the Excel file
+                
                 Cell longestCell = row.createCell(1);
                 longestCell.setCellValue(longestOption);
                 Cell shortestCell = row.createCell(2);
                 shortestCell.setCellValue(shortestOption);
             }
 
-            // Save the changes to the Excel file
+         
             workbook.write(fos);
         } catch (Exception e) {
             e.printStackTrace();
